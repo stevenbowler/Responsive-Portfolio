edfb8a4cddabba97ed04cdffc0784f05f6992839
Steven Bowler <stevenbowler@yahoo.com>
2020-02-04 17:21:19 -0600

    add search to repository

---
+++ javascript.js
@@ -0,0 +1,5 @@
+search = () => {
+    var x = document.getElementById("googlesearch").value;
+    // var googleSearchString = "https://github.com/stevenbowler/Responsive-Portfolio/search?q=" + x + "&unscoped_q=" + x;
+    console.log(x);
+}
