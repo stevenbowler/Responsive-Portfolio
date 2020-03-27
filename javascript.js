89d05e108a5c890f60865056ab939eb8368a79f4
Steven Bowler <stevenbowler@yahoo.com>
2020-03-27 14:03:50 -0500

    eliminate contact.html, include in index.html, cleaner page load

--- javascript.js
+++ javascript.js
@@ -1,5 +1,32 @@
-search = () => {
-    var x = document.getElementById("googlesearch").value;
-    // var googleSearchString = "https://github.com/stevenbowler/Responsive-Portfolio/search?q=" + x + "&unscoped_q=" + x;
-    console.log(x);
+var home = document.getElementById("home");
+var contact = document.getElementById("contact");
+
+const loadHome = () => {
+    contact.setAttribute("style", "display: none;");
+    home.setAttribute("style", "display: none;");
+    home.setAttribute("style", "display: block;");
+    // animateCSS("#home", "zoomOutRight", function () {
+    //     animateCSS("#home", "zoomInRight");
+    // });
+}
+
+const loadContact = () => {
+    home.setAttribute("style", "display: none;");
+    contact.setAttribute("style", "display: block;");
+
+
+}
+
+const animateCSS = (element, animationName, callback) => {
+    const node = document.querySelector(element)
+    node.classList.add('animated', animationName)
+
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName)
+        node.removeEventListener('animationend', handleAnimationEnd)
+
+        if (typeof callback === 'function') callback()
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
 }
